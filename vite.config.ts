dcd6a23857083b431fc4ecaa4f85156b4be14705
Damir <gazdamir00@gmail.com>
2025-03-25 21:26:17 +0300

    test build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/js/[name].js",
+        assetFileNames: "assets/css/[name].[ext]",
+      },
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
